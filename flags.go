3e2b6b778d1fc4f1384705ab698e324a06ec832c
Julian Skupnjak <j.skupnjak@web.de>
2017-07-18 11:27:37 +0200

    increase versions in README and flags.go

--- flags.go
+++ flags.go
@@ -12,7 +12,7 @@ func setCli() *cli.App {
 	}
 	cmd.Name = "Aviator"
 	cmd.Usage = "Navigate to a aviator.yml file and run aviator"
-	cmd.Version = "0.1.0"
+	cmd.Version = "0.3.0"
 	cmd.Flags = getFlags()
 	return cmd
 }
