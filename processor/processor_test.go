1671a33fa2912df0b80a2d007ad4c3de37dcd5f6
Julian Skupnjak <j.skupnjak@web.de>
2017-07-27 14:11:01 +0200

    update

--- processor/processor_test.go
+++ processor/processor_test.go
@@ -22,18 +22,58 @@ var _ = Describe("Processor", func() {
     regexp: ".*.(yml)"
     in_dir: /path/
     skip_non_existing: true
-  - with_in: /path/to/dir/
-    except: true
-  to: result.yml`
+  - with_in: path/to/dir/
+    except:
+    - file2.yml
+  to: result.yml
+
+- base: result.yml
+  merge:
+  - with_in: another/path/
+  for_each_in: path/to/dir/
+  except:
+  - file2.yml
+  regexp: ".*.(yml)"
+  to_dir: some/tmp/dir/
+
+- base: some/tmp/dir/file1.yml
+  merge:
+  - with_in: path/
+  for_each:
+  - foo.yml
+  - bar.yml
+  to_dir: foo/bar/
+
+- base: base.yml
+  prune:
+  - some
+  - properties
+  merge:
+  - with_in: foo/
+  walk_through: foo/bar/
+  to_dir: final/`
 		})
 
 		Context("aviator.yml is valid", func() {
-			It("returns a processor object with specified configuration", func() {
-				processor, err := New([]byte(aviatorYaml))
-				Expect(err).ToNot(HaveOccurred())
+			Context("spruce section", func() {
+				It("returns a processor object with specified configuration", func() {
+					processor, err := New([]byte(aviatorYaml))
+					Expect(err).ToNot(HaveOccurred())
 
-				Expect(len(processor.Aviator.Spruce[0].Merge[0].With.Files)).To(Equal(2))
+					Expect(len(processor.Aviator.Spruce[0].Merge[0].With.Files)).To(Equal(2))
+					Expect(processor.Aviator.Spruce[0].Merge[1].WithIn).To(Equal("path/to/dir/"))
+					Expect(len(processor.Aviator.Spruce[0].Merge[1].Except)).To(Equal(1))
+					Expect(processor.Aviator.Spruce[0].Merge[0].Regexp).To(Equal(".*.(yml)"))
+					Expect(processor.Aviator.Spruce[0].Merge[0].Skip).To(Equal(true))
+					Expect(processor.Aviator.Spruce[0].To).To(Equal("result.yml"))
+					Expect(len(processor.Aviator.Spruce[2].ForEach)).To(Equal(2))
+					Expect(processor.Aviator.Spruce[1].ForEachIn).To(Equal("path/to/dir/"))
+					Expect(len(processor.Aviator.Spruce[1].Except)).To(Equal(1))
+
+				})
+
 			})
+
 		})
 
 	})
